a74c7a90808cbbfeeba6441186682b4a0e0867a2
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-01 18:46:20 -0500

    getting stated

---
+++ main.js
@@ -0,0 +1,27 @@
+function getRandomNumber () {
+    num = Math.random();
+    console.log(num);
+    document.getElementById('d20-output').innerHTML = num;
+}
+
+// rolls a number between 1 - 20
+function getRandomD20 () {
+    num = Math.floor(Math.random() * 20) + 1;
+    console.log(num);
+    document.getElementById('d20-output').innerHTML = num;
+}
+
+// rolls a number between 1 - 6
+function getRandomD6 () {
+    num = Math.floor(Math.random() * 6) + 1;
+    console.log(num);
+    document.getElementById('d6-output').innerHTML = num;
+}
+
+// rolls a number between 1 - 4
+function getRandomD4 () {
+    num = Math.floor(Math.random() * 4) + 1;
+    console.log(num);
+    document.getElementById('d4-output').innerHTML = num;
+}
+
