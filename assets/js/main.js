f430015fb9b25ecdcd512f59e86a84d79663fa6f
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-19 18:00:18 -0500

    workign on d20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
 // rolls a number between 1 - 20
-function getRandomD20 () {
+function getRandomD20() {
+    var critHit = new Audio('/assets/audio/mixkit-dagger-woosh-1487.mp3')
+    var critFail = new Audio('/assets/audio/mixkit-falling-empty-can-389.wav')
+
+
     num = Math.floor(Math.random() * 20) + 1;
-    console.log("D20: " + num);
+
+    if (num == 20) {
+        console.log("D20: " + num);
+        console.log("NAT20");
+        critHit.play();
+    } else if (num == 1) {
+        console.log("D20: " + num);
+        console.log("NAT1");
+        critFail.play();
+    } else
+        console.log("D20: " + num);
     document.getElementById('d20-output').innerHTML = num;
 }
 
 // rolls a number between 1 - 6
-function getRandomD6 () {
+function getRandomD6() {
     num = Math.floor(Math.random() * 6) + 1;
     console.log("D6: " + num);
     document.getElementById('d6-output').innerHTML = num;
 }
 
 // rolls a number between 1 - 4
-function getRandomD4 () {
+function getRandomD4() {
     num = Math.floor(Math.random() * 4) + 1;
     console.log("D4: " + num);
     document.getElementById('d4-output').innerHTML = num;
