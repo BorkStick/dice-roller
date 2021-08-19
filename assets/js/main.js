4f7a435e0a47908d6670bbc05cdfdb8d930aa4ae
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-19 17:45:18 -0500

    making some progress witht the css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,20 @@
-function getRandomNumber () {
-    num = Math.random();
-    console.log(num);
-    document.getElementById('d20-output').innerHTML = num;
-}
-
 // rolls a number between 1 - 20
 function getRandomD20 () {
     num = Math.floor(Math.random() * 20) + 1;
-    console.log(num);
+    console.log("D20: " + num);
     document.getElementById('d20-output').innerHTML = num;
 }
 
 // rolls a number between 1 - 6
 function getRandomD6 () {
     num = Math.floor(Math.random() * 6) + 1;
-    console.log(num);
+    console.log("D6: " + num);
     document.getElementById('d6-output').innerHTML = num;
 }
 
 // rolls a number between 1 - 4
 function getRandomD4 () {
     num = Math.floor(Math.random() * 4) + 1;
-    console.log(num);
+    console.log("D4: " + num);
     document.getElementById('d4-output').innerHTML = num;
 }
-
